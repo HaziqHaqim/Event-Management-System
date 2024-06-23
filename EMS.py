9a7decbfca9078937905163e3b71315cc3b9d026
HaziqHaqim <s231501807@studentmail.unimap.edu.my>
2024-06-23 17:58:47 +0800

    Update EMS.py
    location & info

--- EMS.py
+++ EMS.py
@@ -8,13 +8,14 @@ def createPushed():
     name = name_entry.get()
     phone = phone_entry.get()
     date = date_entry.get()
+    location = location_entry.get()
     info = info_entry.get()
 
-    if not all([name, phone, date]):
+    if not all([name, phone, date, location, info]):
         messagebox.showwarning("Error", "All fields are required")
         return
     
-    event_info = f"{name} - {phone} - {date}"
+    event_info = f"{name} - {phone} - {date} - {location} - {info}"
     event_listbox.insert(END, event_info)
     clearPushed()
 
@@ -22,11 +23,12 @@ def clearPushed():
     name_entry.delete(0, END)
     phone_entry.delete(0, END)
     date_entry.delete(0, END)
-    info_entry.delete(END)
+    location_entry.delete(0,END)
+    info_entry.delete(0,END)
 
 app=Tk()
 app.title("Event Management System")
-app.geometry("530x500")
+app.geometry("530x530")
 
 appLabel = Label(app,text="UniMap Event Booking",font=("Cooper Black", 16))
 appLabel.grid(row=0, column=1, columnspan=1, padx=10, pady=10)
@@ -43,21 +45,25 @@ Label(app, text="Date:").grid(row=3, column=0, padx=10, pady=5)
 date_entry = Entry(app, width=50)
 date_entry.grid(row=3, column=1, padx=10, pady=5)
 
-Label(app, text="Event Info:").grid(row=4, column=0, padx=10, pady=5)
+Label(app, text="Location:").grid(row=4, column=0, padx=10, pady=5)
+location_entry = Entry(app, width=50)
+location_entry.grid(row=4, column=1, padx=10, pady=5)
+
+Label(app, text="Event Info:").grid(row=5, column=0, padx=10, pady=5)
 info_entry = Entry(app, width=50)
-info_entry.grid(row=4, column=1, padx=10, pady=5)
+info_entry.grid(row=5, column=1, padx=10, pady=5)
 
 buttonFrame = Frame(app)
 buttonFrame.grid(row=1, column=1, columnspan=3, pady=10)
 
-appButton = Button(app,text="Create",command=createPushed, width=10).grid(row=5, column=0, padx=10, pady=5)
-appButton = Button(app,text="Update",width=10).grid(row=5, column=1, padx=10, pady=5)
-appButton = Button(app,text="Clear",command=clearPushed,width=10).grid(row=5, column=2, padx=10, pady=5)
-appButton = Button(app,text="Exit",command=exitPushed,width=10).grid(row=6, column=1, padx=10, pady=5)
-
+appButton = Button(app,text="Create",command=createPushed, width=10).grid(row=6, column=0, padx=10, pady=5)
+appButton = Button(app,text="Update",width=10).grid(row=6, column=1, padx=10, pady=5)
+appButton = Button(app,text="Clear",command=clearPushed,width=10).grid(row=6, column=2, padx=10, pady=5)
+appButton = Button(app,text="Exit",command=exitPushed,width=10).grid(row=9, column=2, padx=10, pady=5)
+appButton = Button(app,text="Delete",width=10).grid(row=9, column=0, padx=10, pady=5)
 
 event_listbox = Listbox(app, width=60, height=13)
-event_listbox.grid(row=7, column=0, columnspan=3, padx=10, pady=10)
+event_listbox.grid(row=8, column=0, columnspan=3, padx=10, pady=10)
 
 app.mainloop()
 
